f9a5de2e6e334ea77a82c7b451fa9f44c9b651f4
SuphakornK <161708567+SuphakornK@users.noreply.github.com>
2025-04-27 01:53:20 +0700

    Update variables

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const apiKey = process.env.TMDB_API_KEY;
-const Authorization = process.env.TMDB_API_AUTH;
+const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+const Authorization = process.env.NEXT_PUBLIC_TMDB_API_AUTH;
 
 export const tmdbApi = axios.create({
   baseURL: "https://api.themoviedb.org/3",
